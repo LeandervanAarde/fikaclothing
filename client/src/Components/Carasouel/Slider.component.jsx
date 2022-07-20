3f875ab5aeca1e58b995cd5c8b19fc7785cef933
LeandervanAarde <200211@virtualwindow.co.za>
2022-07-20 21:18:36 +0200

    Content added
    Content has been added to the slider

--- client/src/Components/Carasouel/Slider.component.jsx
+++ client/src/Components/Carasouel/Slider.component.jsx
@@ -1,30 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Sliderdots from '../CarasouelDots/Sliderdots.component';
 import './Slider.styles.scss'
 import sliderImages from '../../MockImages/mockimages';
 
+
 const Slider = () => {
     const images = sliderImages;
     const [img, setImg] = useState(0);
-    const shoes = images.map(i =>(i.shoe))
+    const shoes = images.map(i => (i.shoe));
+    const heading= images.map(i => (i.title));
+    const content = images.map(i => (i.content))
+    const dots = images.map(i => (i.length));
 
-    useEffect(() =>{
-        let counter = 0;     
-        const timer = setTimeout(() =>{
-            img == shoes.length -1 ? setImg(0) : setImg(img +1)
-        }, 3000)
-    },[img])
+    console.log(shoes.length - 1)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            img == shoes.length - 1 ? setImg(0) : setImg(img + 1)
+        }, 4500)
+    }, [img])
 
     console.log(img)
 
-   
+
     return (
-        <div className='slider-container'>
+
+        <div className='slider-container' style={{ backgroundImage: `url(${shoes[img]})` }}>
             <div className='overlay'>
-          
+                <h1 className='introduction'>{heading[img]}</h1>
+                <p className='content'>{content[img]}</p>
             </div>
-            <img src={shoes[img]}/>
         </div>
+
     );
 };
 
