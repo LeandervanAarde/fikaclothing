dc2b558bd3f5bf232ff3c77303482ab7797acf0c
LeandervAarde <103223863+LeandervanAarde26@users.noreply.github.com>
2024-07-12 15:18:43 +0200

    feature(filter): Filter structure added
    Filter structure has been added.

--- client/src/Routes/Shop/Shop.mui.tsx
+++ client/src/Routes/Shop/Shop.mui.tsx
@@ -1,22 +1,40 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import ProductCard from "../../components/Card/Card.mui";
 import CardGrid from "../../components/CardGrid/CardGrid.mui";
+import Filter from "../../features/ProductFilter/Filter.mui";
+import { globalFonts } from "../../StylesSource/styles";
 
+type Props = {};
 
-type Props = {
-
-};
-
-export default function ProductsSection({ }: Props) {
-    const mappedItems: number[] = [1,2,3,4,5,6,7,8,34,3,2,34,45,3,4,5,6,7,7,6]
+export default function ProductsSection({}: Props) {
+  const mappedItems: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 34, 3, 2, 34, 45, 3, 4, 5, 6, 7, 7, 6,
+  ];
   return (
-    <CardGrid heading="Feature Brand">
-    {mappedItems.map((item) => (
-      <Grid item xs={11} sm={5} md={4} lg={2.4} xl={2.4}>
-        <ProductCard /> {/* Assuming item contains necessary props */}
-      </Grid>
-    ))}
-  </CardGrid>
+    <>
+      <Container 
+
+          maxWidth={false}
+          sx={{
+            pt: 2,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+      >
+        <Typography variant="h4" sx={{fontFamily: globalFonts.body}}> 
+          Our Products
+        </Typography>
+        <Filter/>
+      </Container>
+      <CardGrid>
+        {mappedItems.map((item) => (
+          <Grid item xs={11} sm={5} md={4} lg={2.4} xl={2.4}>
+            <ProductCard /> {/* Assuming item contains necessary props */}
+          </Grid>
+        ))}
+      </CardGrid>
+    </>
   );
 }
